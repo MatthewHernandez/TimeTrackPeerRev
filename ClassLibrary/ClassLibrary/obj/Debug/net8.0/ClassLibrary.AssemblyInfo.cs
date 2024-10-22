4546238f837e060dfa5e33883b6d6f8926459d4e
Jesus Barrera-Gilabert III <barrerajesus224@gmail.com>
2024-10-22 16:01:37 -0500

    Modified project name to "G1_Library" and put all code into a namespace with the same name

--- ClassLibrary/ClassLibrary/obj/Debug/net8.0/ClassLibrary.AssemblyInfo.cs
+++ ClassLibrary/ClassLibrary/obj/Debug/net8.0/ClassLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b28c2bde9eda0de10129a74d7d47155469dc9a44")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
