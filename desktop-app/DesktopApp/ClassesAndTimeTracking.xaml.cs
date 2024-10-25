aa7e03a5beb75bd7a18c9cfe71817b739d984187
WilliamLaFoy <juicyjuicyfruitbeast@gmail.com>
2024-10-25 11:02:50 -0500

    Peer reviews
    base peer review ui and functionalities

--- desktop-app/DesktopApp/ClassesAndTimeTracking.xaml.cs
+++ desktop-app/DesktopApp/ClassesAndTimeTracking.xaml.cs
@@ -9,12 +9,14 @@ namespace DesktopApp
 {
     public partial class ClassesAndTimeTracking : ContentPage
     {
-        //written by William LaFoy for CS4485, Project, 10/9/2024-10/18/2024. NetID wel190000
+        //written by William LaFoy for CS4485, Project, 10/9/2024-10/25/2024. NetID wel190000
         /*
          Base framework for listing out classes, listing out associated students and cummulative hours
          and specific dates with those hours.
 
         10/18 now allows you to select cells with ease and modify the date hour etc.
+
+        10/25 now displays peer reviews, button to make a new peer review exists, but functionality will depend on sql calls.
          */
 
         // Sample data structure for student hours
@@ -25,47 +27,11 @@ namespace DesktopApp
 
             public int TeamNumber { get; set; }
             public decimal Hours { get; set; }
+
+           // public string studentinfo { get; set; }
         }
 
-        // Updated StudentHourDetail with INotifyPropertyChanged
-        public class StudentHourDetail : INotifyPropertyChanged
-        {
-            private DateTime _date;
-            private decimal _hours;
-
-            public DateTime Date
-            {
-                get => _date;
-                set
-                {
-                    if (_date != value)
-                    {
-                        _date = value;
-                        OnPropertyChanged(nameof(Date));
-                    }
-                }
-            }
-
-            public decimal Hours
-            {
-                get => _hours;
-                set
-                {
-                    if (_hours != value)
-                    {
-                        _hours = value;
-                        OnPropertyChanged(nameof(Hours));
-                    }
-                }
-            }
-
-            public event PropertyChangedEventHandler PropertyChanged;
-
-            protected void OnPropertyChanged(string propertyName)
-            {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
+       
 
         // Constructor
         public ClassesAndTimeTracking()
@@ -174,8 +140,80 @@ namespace DesktopApp
             if (e.CurrentSelection.Count > 0 && e.CurrentSelection[0] is StudentHour selectedHour)
             {
                 LoadStudentDetails(selectedHour.StudentName);
+                LoadPeerReviews(selectedHour.StudentName); // Load peer reviews for the selected student
             }
         }
+
+
+
+
+
+
+
+
+        /// 
+        /// student details section
+        /// 
+
+
+
+
+
+
+        // Updated StudentHourDetail with INotifyPropertyChanged
+        public class StudentHourDetail : INotifyPropertyChanged
+        {
+            private DateTime _date;
+            private decimal _hours;
+            private string _studentinfo;
+
+            public DateTime Date
+            {
+                get => _date;
+                set
+                {
+                    if (_date != value)
+                    {
+                        _date = value;
+                        OnPropertyChanged(nameof(Date));
+                    }
+                }
+            }
+
+            public decimal Hours
+            {
+                get => _hours;
+                set
+                {
+                    if (_hours != value)
+                    {
+                        _hours = value;
+                        OnPropertyChanged(nameof(Hours));
+                    }
+                }
+            }
+
+            public string studentinfo
+            {
+                get => _studentinfo;
+                set
+                {
+                    if (_studentinfo != value)
+                    {
+                        _studentinfo = value;
+                        OnPropertyChanged(nameof(studentinfo));
+                    }
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected void OnPropertyChanged(string propertyName)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
 
         // Method to load details for the selected student
         private void LoadStudentDetails(string studentName)
@@ -183,13 +221,113 @@ namespace DesktopApp
             // This should be replaced with a database call to get specific dates and hours for the selected student
             var hours = new List<StudentHourDetail>
             {
-                new StudentHourDetail { Date = DateTime.Today.AddDays(-1), Hours = 3 },
-                new StudentHourDetail { Date = DateTime.Today.AddDays(-2), Hours = 2 },
-                new StudentHourDetail { Date = DateTime.Today.AddDays(-3), Hours = 4 }
+                new StudentHourDetail { Date = DateTime.Today.AddDays(-1), Hours = 3, studentinfo = "Did x, y, z" },
+                new StudentHourDetail { Date = DateTime.Today.AddDays(-2), Hours = 2, studentinfo = "Did x, y, z" },
+                new StudentHourDetail { Date = DateTime.Today.AddDays(-3), Hours = 4, studentinfo = "Did x, y, z" }
             };
 
             HoursCollectionView.ItemsSource = hours; // Bind to CollectionView
             StudentDetailStack.IsVisible = true; // Show the details stack
+            PeerReviewStack.IsVisible = true; // Show the peer review stack
         }
+
+
+        
+        ///
+        ///Peer reviews section
+        ///
+        public class PeerReview
+        {
+            public DateTime Timestamp { get; set; } // The deadline or timestamp of the review
+            public int QualityOfWork { get; set; }
+            public int Timeliness { get; set; }
+            public int Communication { get; set; }
+            public int Teamwork { get; set; }
+            public int EffortParticipation { get; set; }
+
+            public List<TeamMemberComment> TeamMemberComments { get; set; } // Comments for each team member
+        }
+
+        public class TeamMemberComment
+        {
+            public string TeammateName { get; set; }
+            public string Comment { get; set; }
+        }
+
+
+
+        // Loading peer reviews for a student
+        private void LoadPeerReviews(string studentName)
+        {
+            // Sample peer reviews for demonstration
+            var peerReviews = new List<PeerReview>
+    {
+        new PeerReview
+        {
+            Timestamp = DateTime.Today.AddDays(-10),
+            QualityOfWork = 5,
+            Timeliness = 5,
+            Communication = 5,
+            Teamwork = 5,
+            EffortParticipation = 5,
+            TeamMemberComments = new List<TeamMemberComment>
+            {
+                new TeamMemberComment { TeammateName = "William LaFoy", Comment = "Excellent work!" },
+                new TeamMemberComment { TeammateName = "abc xyz", Comment = "Needs improvement." }
+            }
+        },
+        new PeerReview
+        {
+            Timestamp = DateTime.Today.AddDays(-20),
+            QualityOfWork = 4,
+            Timeliness = 3,
+            Communication = 4,
+            Teamwork = 4,
+            EffortParticipation = 4,
+            TeamMemberComments = new List<TeamMemberComment>
+            {
+                new TeamMemberComment { TeammateName = "William LaFoy", Comment = "Solid teamwork." },
+                new TeamMemberComment { TeammateName = "abc xyz", Comment = "Great communication." }
+            }
+        }
+    };
+
+            // Bind peer reviews to PeerReviewsCollectionView
+            PeerReviewsCollectionView.ItemsSource = peerReviews;
+        }
+
+        // Event handler for selecting a peer review
+        private void PeerReviewsCollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection.Count > 0 && e.CurrentSelection[0] is PeerReview selectedReview)
+            {
+                // Bind teammates in selected review to TeammatesCollectionView
+                TeammatesCollectionView.ItemsSource = selectedReview.TeamMemberComments;
+                PeerReviewDetailsStack.BindingContext = selectedReview;
+                PeerReviewDetailsStack.IsVisible = false; // Hide details until teammate is selected
+            }
+        }
+
+        // Event handler for selecting a teammate
+        private void TeammatesCollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection.Count > 0 && e.CurrentSelection[0] is TeamMemberComment selectedTeammate)
+            {
+                PeerReviewDetailsStack.IsVisible = true;
+
+                // Bind comments to CommentsCollectionView for the selected teammate
+                CommentsCollectionView.ItemsSource = (PeerReviewDetailsStack.BindingContext as PeerReview)?.TeamMemberComments;
+            }
+        }
+
+
+
+        private void AddNewPeerReviewButton_Clicked(object sender, EventArgs e)
+        {
+            // Logic to add a new peer review
+            
+           
+        }
+
     }
 }
