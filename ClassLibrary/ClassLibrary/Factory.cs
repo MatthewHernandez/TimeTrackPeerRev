85a94f9bc035c967f053d18c397421fc1a0ff915
Jesus Barrera-Gilabert III <barrerajesus224@gmail.com>
2024-11-07 13:35:31 -0600

    Implemented the factory classes for PeerRevEntry and TimeEntry

--- ClassLibrary/ClassLibrary/Factory.cs
+++ ClassLibrary/ClassLibrary/Factory.cs
@@ -1,18 +1,18 @@
 ﻿/* Factory contains functions for creating and destroying Student and Professor entities and propagating
  * those functions onto the database.
  * Author:  Jesus Barrera-Gilabert III
- * Date:    10/29/2024
+ * Date:    11/07/2024
  * Class:   Computer Science Project CS 4485.0W1
  * Net ID:  jxb171030
  * UTD ID:  2021348532
- * Version: 0.2
+ * Version: 0.3
  */
 
 using Microsoft.Data.SqlClient;
 
 namespace G81_Library
 {
-    //Creates and stores instances of Students and Professors in the database
+    // Creates and stores instances of Students and Professors in the database
     class PersonFactory
     {
         private string _SqlCon;
@@ -27,7 +27,7 @@ namespace G81_Library
             _ProfessorDB = professorDB;
         }
 
-        //Creates a Student entity and stores it in the database
+        // Creates a Student entity and stores it in the database; return the created instance
         Student? CreateStudent(string fname, string lname, string netID, int utdID, float cID, int group)
         {
             string sCom = "INSERT INTO" + _StudentDB + "([FirstName], [LastName], [NetID], [UTDID], [Password], [CID], [Group]) VALUES(@first, @last, @net, @utd, @pass, @c, @group)";
@@ -70,7 +70,7 @@ namespace G81_Library
                 return new Student(fname, lname, utdID, netID, cID, group);
         }
 
-        //Creates a Professor entity and stores it in the database
+        // Creates a Professor entity and stores it in the database; return the created instance
         Professor? CreateProfessor(string fname, string lname, string netID, int utdID)
         {
             string pCom = "INSERT INTO" + _ProfessorDB + "([FirstName], [LastName], [NetID], [UTDID], [Password]) VALUES(@first, @last, @net, @utd, @pass)";
@@ -110,6 +110,118 @@ namespace G81_Library
             }
 
             return new Professor(fname, lname, utdID, netID);
+        }
+    }
+
+    // Create and store instances of PeerRevEntry in the database
+    class PeerFactory
+    {
+        private string _sqlCon;
+        private string _peerDB;
+
+        // Constructor
+        public PeerFactory(string sqlCon, string peerDB)
+        {
+            _sqlCon = sqlCon;
+            _peerDB = peerDB;
+        }
+
+        // Create a PeerRevEntry intance and store in the database; return the created instance
+        PeerRevEntry? CreateReview(Student reviewer, Student reviewee, string comment, DateOnly start, DateOnly end, int rank)
+        {
+            string pCom = "INSERT INTO" + _peerDB + "([ReviewerID], [RevieweeID], [Comment], [Start], [End], [Rank]) VALUE(@rr, @re, @com, @s, @e, @rank)";
+
+            using (SqlConnection cnn = new SqlConnection(_sqlCon))
+            {
+                try
+                {
+                    cnn.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(pCom))
+                    {
+                        try
+                        {
+                            cmd.Parameters.AddWithValue("@rr", reviewer.UtdID);
+                            cmd.Parameters.AddWithValue("@re", reviewee.UtdID);
+                            cmd.Parameters.AddWithValue("@com", comment);
+                            cmd.Parameters.AddWithValue("@s", start);
+                            cmd.Parameters.AddWithValue("@e", end);
+                            cmd.Parameters.AddWithValue("@rank", rank);
+
+                            if (cmd.ExecuteNonQuery() <= 0)
+                            {
+                                return null;
+                            }
+                        }
+                        catch
+                        {
+                            throw new Exception("CreateReview: Failed to Create SqlCommand Object");
+                        }
+                    }
+                }
+                catch
+                {
+                    throw new Exception("CreateReview: Failed to Create SqlConnection Object");
+                }
+            }
+
+            return new PeerRevEntry(reviewer, reviewee, rank, comment, start, end);
+        }
+    }
+
+    // Create and store instances of TimeEntry in the database
+    class TimeFactory
+    {
+        private string _sqlCon;
+        private string _timeDB;
+
+        // Constructor 
+        public TimeFactory(string sqlCon, string timeDB)
+        {
+            _sqlCon = sqlCon;
+            _timeDB = timeDB;
+        }
+
+        // Create TimeEntry instance and store in the database; return the created instance
+        TimeEntry? CreateTime(Student stu, TimeSpan time, DateOnly date, string prob, string plan, string acc)
+        {
+            string tCom = "INSERT INTO" + _timeDB + "([StudentID], [Time], [Date], [Problems], [Plan], [Accomplished]) VALUES(@stu, @time, @date, @prob, @plan, @acc)";
+
+            using (SqlConnection cnn = new SqlConnection(_sqlCon))
+            {
+                try
+                {
+                    cnn.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(tCom))
+                    {
+                        try
+                        {
+                            cmd.Parameters.AddWithValue("@stu", stu.UtdID);
+                            cmd.Parameters.AddWithValue("@time", time);
+                            cmd.Parameters.AddWithValue("@date", date);
+                            cmd.Parameters.AddWithValue("@prob", prob);
+                            cmd.Parameters.AddWithValue("@plan", plan);
+                            cmd.Parameters.AddWithValue("@acc", acc);
+
+                            if(cmd.Parameters.Count <= 0)
+                            {
+                                return null;
+                            }
+                        }
+                        catch
+                        {
+                            throw new Exception("CreateTime: Failed to Create SqlCommand Object");
+                        }
+                    }
+                }
+                catch
+                {
+                    throw new Exception("CreateTime: Failed to Create SqlConnection Object");
+                }
+            }
+
+            return new TimeEntry(stu, time, date, prob, plan, acc);
         }
     }
 }
